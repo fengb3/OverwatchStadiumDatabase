f7a23eb1de59b7ecb5bd07b4be4d354d013e83df
Bohan <fengb3@gmail.com>
2025-12-25 10:22:40 +0800

    Update PlaywrightDI.cs

--- OverwatchStadiumDatabase.Worker/DependencyInjection/PlaywrightDI.cs
+++ OverwatchStadiumDatabase.Worker/DependencyInjection/PlaywrightDI.cs
@@ -16,22 +16,32 @@ public static class Playwright
         {
             var playwright = provider.GetRequiredService<IPlaywright>();
             var isCi = Environment.GetEnvironmentVariable("CI") == "true";
-            var browser = playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
-            {
-                Headless = isCi,
-                Args = ["--disable-blink-features=AutomationControlled"]
-            }).GetAwaiter().GetResult();
+            var browser = playwright
+                .Chromium.LaunchAsync(
+                    new BrowserTypeLaunchOptions
+                    {
+                        Headless = isCi,
+                        Args = ["--disable-blink-features=AutomationControlled"],
+                    }
+                )
+                .GetAwaiter()
+                .GetResult();
             return browser;
         });
 
         services.AddSingleton<IBrowserContext>(provider =>
         {
             var browser = provider.GetRequiredService<IBrowser>();
-            var context = browser.NewContextAsync(new BrowserNewContextOptions
-            {
-                UserAgent =
-                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
-            }).GetAwaiter().GetResult();
+            var context = browser
+                .NewContextAsync(
+                    new BrowserNewContextOptions
+                    {
+                        UserAgent =
+                            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+                    }
+                )
+                .GetAwaiter()
+                .GetResult();
             return context;
         });
 
@@ -41,7 +51,6 @@ public static class Playwright
             var page = context.NewPageAsync().GetAwaiter().GetResult();
             return page;
         });
-
 
         return services;
     }
